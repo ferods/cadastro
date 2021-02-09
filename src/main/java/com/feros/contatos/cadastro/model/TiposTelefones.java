a4ced51b9198f752f51b5b936623883b10b9847b
ferods <felipe.rodriguessn@gmail.com>
2021-02-09 11:13:03 -0300

    atualização de correção do código save de pessoas

--- src/main/java/com/feros/contatos/cadastro/model/TiposTelefones.java
+++
@@ -1,7 +0,0 @@
-package com.feros.contatos.cadastro.model;
-
-public enum TiposTelefones {
-	
-	RESIDENCIAL, COMERCIAL, CELULAR
-
-}
--- src/main/java/com/feros/contatos/cadastro/model/Uf.java
+++
@@ -1,7 +0,0 @@
-package com.feros.contatos.cadastro.model;
-
-public enum Uf {
-	
-	AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO
-
-}
